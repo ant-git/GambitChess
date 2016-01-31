bbf74f3ed394a6b7431c773237ffb1256900f494
antant <antonkhvdv@gmail.com>
2016-01-31 17:54:23 +1300

    Simple css added

--- src/game/Main.java
+++ src/game/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setTitle("Gambit Chess");
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/main.css");
         primaryStage.setScene(scene);
 
         primaryStage.show();
